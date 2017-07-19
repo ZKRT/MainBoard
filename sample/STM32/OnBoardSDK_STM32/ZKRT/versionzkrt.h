86739b474c4d9d8a7d560bfb47644a71e01dd51b
yanly <413899391@qq.com>
2017-07-19 15:26:07 +0800

    add Release/MainBoard-V02-00-170719; version change!

--- sample/STM32/OnBoardSDK_STM32/ZKRT/versionzkrt.h
+++ sample/STM32/OnBoardSDK_STM32/ZKRT/versionzkrt.h
@@ -33,7 +33,7 @@
 
 #define  PRODUCT_NAME													"MainBoard"
 #define  PRODUCT_ID	                          "10000000"
-#define  PRODUCT_VERSION                      "V01-01"
+#define  PRODUCT_VERSION                      "V02-00"
 /* Exported functions ------------------------------------------------------- */
 
 #endif /* __VERSIONZKRT_H */
